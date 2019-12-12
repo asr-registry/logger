2cae31b1feb4a833d2e896d2011a8c4085c19e1e
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-12 09:19:07 +0430

    #1

---
+++ src/test/java/af/asr/logger/PhoenixLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
